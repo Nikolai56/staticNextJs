170a4321846cfe9e372a0a4215ab80c9bd837bfc
Nikolai56 <>
2021-03-03 15:39:40 +0300

    update config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 const { i18n } = require('./next-i18next.config')
 
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config()
-}
-
 module.exports = {
-  env: {
-    DEFAULT_LOCALE: process.env.DEFAULT_LOCALE,
-  },
   i18n,
 }
